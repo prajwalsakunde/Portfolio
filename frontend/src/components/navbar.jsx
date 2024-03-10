9350f00bd91870d03245ec966473b2995f38d158
Prajwal Sakunde <prajwalsakunde1234@gmail.com>
2024-03-10 17:22:26 +0530

    new changes in navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,7 +1,9 @@
 import { NavLink, Link } from "react-router-dom";
 import { useState } from "react";
-import { FiChevronDown } from "react-icons/fi";
+import { FiChevronDown, FiCoffee } from "react-icons/fi";
 import prajwals from '../assets/prajwals.png';
+import { SiBuymeacoffee } from "react-icons/si";
+import coffeecup from '../assets/coffee-cup.png';
 
 function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -24,7 +26,7 @@ function NavBar() {
       <div className="flex items-center"> 
         <Link to="/" className="flex items-center">
           <img src={prajwals} className="w-26 h-8" alt="Logo" />
-          <div className="font-black px-2 pt-4"> .</div>
+          <div className="font-black px-2 pt-4">.</div>
         </Link>
       </div>
       <div className="sm:hidden">
@@ -38,37 +40,46 @@ function NavBar() {
       <div
         className={`${
           isMenuOpen ? "flex" : "hidden"
-        } flex-col sm:flex-row items-center space-y-1 sm:space-y-0  sm:flex `}
+        } flex-col sm:flex-row px-1 items-center space-y-1 sm:space-y-0 gap-x-4 sm:flex `}
       > 
-    
-        <NavLink
-          to="/event"
-          className={`text-lg font-extralight hover:font-semibold hover:text-black mr-0 sm:ml-10  sm:mr-32 ${
-            activeTab === "event" ? "text-black font-semibold" : "text-lg font-extralight text-black"
-          }`}
-          onClick={() => handleNavLinkClick("event")}
-        >
-          Event
-        </NavLink>
-        <NavLink
-          to="/members"
-          className={`text-lg font-extralight hover:font-semibold hover:text-black sm:mr-32 ${
-            activeTab === "members" ? "text-black font-semibold" : "text-lg font-extralight text-black"
-          }`}
-          onClick={() => handleNavLinkClick("members")}
-        >
-        Members
-        </NavLink>
-        
-        <NavLink
-          to="/contact"
-          className={`text-lg font-extralight hover:font-semibold hover:text-black sm:mr-8 ${
-            activeTab === "contact" ? "text-black font-semibold" : "text-lg font-extralight text-black"
-          }`}
-          onClick={() => handleNavLinkClick("contact")}
-        >
-          Contact us
-        </NavLink>
+        <div className="mr-0 sm:ml-10 sm:mr-20">
+          <NavLink
+            to="/resume"
+            className={`text-lg font-extralight hover:font-semibold ${
+              activeTab === "event" ? "text-black font-semibold" : "text-lg font-extralight text-black"
+            }`}
+            onClick={() => handleNavLinkClick("event")}
+            style={{ marginRight: '10px' }} // Added margin-right style
+          >
+            Resume
+          </NavLink>
+        </div>
+        <div className="mr-0 sm:ml-10 sm:mr-20">
+          <NavLink
+            to="/projects"
+            className={`text-lg font-extralight hover:font-semibold hover:text-black ${
+              activeTab === "members" ? "text-black font-semibold" : "text-lg font-extralight text-black"
+            }`}
+            onClick={() => handleNavLinkClick("members")}
+            style={{ marginRight: '10px' }} // Added margin-right style
+          >
+            Projects
+          </NavLink>
+        </div>
+        <div className="mr-0 sm:mx-10">
+          <NavLink
+            to="/contact"
+            className={`text-lg gap-3 font-extralight hover:font-semibold flex flex-row hover:text-black ${
+              activeTab === "contact" ? "text-black font-semibold" : "text-lg font-extralight text-black"
+            }`}
+            onClick={() => handleNavLinkClick("contact")}
+          >
+            
+            {/* <img src={coffeecup} className="w-10 h-10 rotate-12" alt="coffee" /> */}
+            <SiBuymeacoffee className="w-10 h-10 rotate-45" />
+            Hire me
+          </NavLink>
+        </div>
       </div>
     </nav>
   );
--- frontend/src/components/startpage.jsx
+++ frontend/src/components/startpage.jsx
@@ -3,7 +3,7 @@ const Startpage = () => {
     
     return (
       <div className="">
-      
+          
       </div>
     );
 
